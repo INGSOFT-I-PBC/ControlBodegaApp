367488ae708a1031cc5b8e937ab2dc49f537ae65
Neoterux <labfernandez2014@gmail.com>
2022-09-11 18:29:35 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    id ("com.android.application")
+    kotlin("android")
+}
+
+android {
+
+    compileSdk = Constants.Android.compileVersion
+
+    defaultConfig {
+
+        applicationId = "com.neoterux.controlbodega"
+        minSdk = Constants.Android.minVersion
+        targetSdk = Constants.Android.targetVersion
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary =  true
+        }
+    }
+
+    buildTypes {
+        named("debug") {
+            isMinifyEnabled = false
+        }
+        named("release") {
+            isMinifyEnabled = true
+            proguardFiles ( getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Constants.composeCompilerVersion
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(app.android.Libraries.androidCore)
+    implementation(app.android.Libraries.activityCompose)
+    implementation(app.android.Libraries.composeUI)
+    implementation(app.android.Libraries.composeMaterial)
+    implementation(app.android.Libraries.lifecycle)
+    implementation(app.android.Libraries.gson)
+    implementation(app.android.Libraries.composePreview)
+    implementation(app.android.Libraries.retrofit)
+
+    testImplementation(app.android.Libraries.junit)
+    androidTestImplementation(app.android.Libraries.androidJunit)
+    androidTestImplementation(app.android.Libraries.espresso)
+    androidTestImplementation(app.android.Libraries.uiTestJunit)
+    debugImplementation(app.android.Libraries.Debug.composeTooling)
+    debugImplementation(app.android.Libraries.Debug.composeTestManifest)
+
+//    implementation 'androidx.core:core-ktx:1.7.0'
+//    implementation "androidx.compose.ui:ui:$compose_version"
+//    implementation "androidx.compose.material:material:$compose_version"
+//    implementation "androidx.compose.ui:ui-tooling-preview:$compose_version"
+//    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.3.1'
+//    implementation 'androidx.activity:activity-compose:1.3.1'
+//    testImplementation 'junit:junit:4.13.2'
+//    androidTestImplementation 'androidx.test.ext:junit:1.1.3'
+//    androidTestImplementation 'androidx.test.espresso:espresso-core:3.4.0'
+//    androidTestImplementation "androidx.compose.ui:ui-test-junit4:$compose_version"
+//    debugImplementation "androidx.compose.ui:ui-tooling:$compose_version"
+//    debugImplementation "androidx.compose.ui:ui-test-manifest:$compose_version"
+}
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.application:com.android.application.gradle.plugin:${Constants.AGPVersion}")
+        classpath ("com.android.library:com.android.library.gradle.plugin:${Constants.AGPVersion}")
+        classpath ("org.jetbrains.kotlin.android:org.jetbrains.kotlin.android.gradle.plugin:${Constants.kotlinVersion}")
+    }
+}
+
+//plugins {
+//    id ("com.android.application") version "${Constants.AGPVersion}"
+//    id ("com.android.library") version "${Constants.AGPVersion}"
+//    id ("org.jetbrains.kotlin.android") version Constants.kotlinAndroidVersion
+//
+////    id 'com.android.application' version '7.2.1' apply false
+////    id 'com.android.library' version '7.2.1' apply false
+////    id 'org.jetbrains.kotlin.android' version '1.5.31' apply false
+//}
+
+tasks.register("clean").configure{
+    delete("build")
+}
